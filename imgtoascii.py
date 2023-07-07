0563a300f0ef41b30ffb25335b9813c48a882bac
Ardnys <semresavas35@gmail.com>
2023-07-07 22:35:34 +0300

    function tidying up and a bare class

--- imgtoascii.py
+++ imgtoascii.py
@@ -7,6 +7,14 @@ import sys
 
 np.set_printoptions(threshold=sys.maxsize)
 
+class Pixel:
+    def __init__(self, coords: list, char: str) -> None:
+        self.coords = coords
+        self.char = char
+
+    def __str__(self) -> str:
+        return f'coords: {self.coords}\nchar: {self.char}'
+    
 
 def get_them_imgs(image_path: str) -> list:
     images = list()
@@ -19,62 +27,6 @@ def get_them_imgs(image_path: str) -> list:
         print("read " + file.name)
 
     return images
-
-def print_inconsole(matrix):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            print(matrix[i][j], end="")
-        print()
-
-def img_plot(images: list):
-    fig, ax = plt.subplots(2, 3)
-
-    img_idx = 0
-    for i in range(2):
-        for j in range(3):
-            if img_idx >= len(images):
-                break
-            ax[i, j].imshow(images[img_idx])
-            img_idx += 1
-
-    plt.show()
-
-def dot_product(vec_1: list, vec_2: list) -> list:
-    if len(vec_1) != len(vec_2):
-        raise Exception("can't dot product different size vectors")
-        # i don't even know if it's true but it feels like it
-    vec = list()
-    for i,j in zip(vec_1, vec_2):
-        vec.append(i * j)
-    
-    return vec
-
-def uh(matrix):
-    cos_theta = int(cos(np.pi/2)) # 45 degrees
-    sin_theta = int(sin(np.pi/2))
-    Rx_matrix = [[1,      0,          0     ],
-                 [0, cos_theta, -1*sin_theta],
-                 [0, sin_theta, cos_theta  ]]
-    
-    column_vectors = list()
-    # add third dimension
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            column_vectors.append([i, j, 0])
-
-    transformed_vectors = list()
-    for x in column_vectors:
-        for i in range(len(Rx_matrix)):
-            c = dot_product(Rx_matrix[i], x)
-            transformed_vectors.append(c)
-            print(f'prev coord: {x}\nnew coord: {c}')
-    # ctr = 0
-    # for i in range(len(matrix)):
-    #     for j in range(len(matrix[0])):
-    #         matrix[i, j] = transformed_vectors[ctr]
-    #         ctr += 1
-
-    return matrix
 
 # just the dots
 def simple_ascii(images):
@@ -110,6 +62,64 @@ def simple_ascii(images):
         rotated_matrix = uh(ascii_matrix)
         # print_inconsole(rotated_matrix)
         # print(rotated_matrix)
+
+
+
+def uh(matrix):
+    cos_theta = int(cos(np.pi/2)) # 45 degrees
+    sin_theta = int(sin(np.pi/2))
+    Rx_matrix = [[1,      0,          0     ],
+                 [0, cos_theta, -1*sin_theta],
+                 [0, sin_theta, cos_theta  ]]
+    
+    column_vectors = list()
+    # add third dimension
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            column_vectors.append([i, j, 0])
+
+    transformed_vectors = list()
+    for x in column_vectors:
+        for i in range(len(Rx_matrix)):
+            c = dot_product(Rx_matrix[i], x)
+            transformed_vectors.append(c)
+            print(f'prev coord: {x}\nnew coord: {c}')
+    # ctr = 0
+    # for i in range(len(matrix)):
+    #     for j in range(len(matrix[0])):
+    #         matrix[i, j] = transformed_vectors[ctr]
+    #         ctr += 1
+
+def dot_product(vec_1: list, vec_2: list) -> list:
+    if len(vec_1) != len(vec_2):
+        raise Exception("can't dot product different size vectors")
+        # i don't even know if it's true but it feels like it
+    vec = list()
+    for i,j in zip(vec_1, vec_2):
+        vec.append(i * j)
+    
+    return vec
+
+def print_inconsole(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            print(matrix[i][j], end="")
+        print()
+
+def img_plot(images: list):
+    fig, ax = plt.subplots(2, 3)
+
+    img_idx = 0
+    for i in range(2):
+        for j in range(3):
+            if img_idx >= len(images):
+                break
+            ax[i, j].imshow(images[img_idx])
+            img_idx += 1
+
+    plt.show()
+
+
 
 if __name__ == '__main__':
     # i guess i'd rather public static void main string args
