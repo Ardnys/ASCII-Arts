7c1ecbaa32624b67710b24e9014afa46187bd78f
Ardnys <semresavas35@gmail.com>
2023-07-12 22:58:09 +0300

    i broke everything and i have more ideas

--- imgtoascii.py
+++ imgtoascii.py
@@ -6,6 +6,22 @@ from math import cos, sin
 import sys
 
 np.set_printoptions(threshold=sys.maxsize)
+'''
+TODO
+cartesian coordinate system
+-----------------------
+i kinda try to mash the matrix indices and coordinates together and it's just a mess
+so i need a CCS representation of the image. probs a field too
+then the transformation between the two. given the indices of the matrix, 
+where does it land on the CCS?
+
+essentially i need to represent a 2D cartesian space.
+verify this my drawing dots and lines.
+maybe first rotate the dot and a line.
+of course lines aren't smooth. just / or consecutive dots.
+
+
+'''
 
 class Pixel:
     def __init__(self, coords: list, char: str) -> None:
@@ -14,6 +30,12 @@ class Pixel:
 
     def __str__(self) -> str:
         return f'coords: {self.coords}\nchar: {self.char}'
+    
+    def set_coords(self, coords: list):
+        self.coords = coords
+    
+    def set_char(self, char: str):
+        self.char = char
     
 
 def get_them_imgs(image_path: str) -> list:
@@ -24,20 +46,20 @@ def get_them_imgs(image_path: str) -> list:
             continue
 
         images.append(iio.imread(file))
-        print("read " + file.name)
+        # print("read " + file.name)
 
     return images
 
 # just the dots
 def simple_ascii(images):
-    img = images[4]
+    img = images[3]
     nrows = len(img)
     ncols = len(img[0])
-    print(f'rows: {nrows} \ncols: {ncols}')
-    ascii_matrix = np.empty((nrows, ncols), dtype=str)
-    mrows = len(ascii_matrix[0])
-    mcols = len(ascii_matrix)
-    print(f'matrix rows: {mrows}\nmatrix cols: {mcols}')
+    # print(f'rows: {nrows} \ncols: {ncols}')
+    ascii_matrix = np.empty((nrows, ncols), dtype=Pixel)
+    # mrows = len(ascii_matrix[0])
+    # mcols = len(ascii_matrix)
+    # print(f'matrix rows: {mrows}\nmatrix cols: {mcols}')
     # print(img)
     # plt.imshow(img)
     # plt.show()
@@ -50,11 +72,11 @@ def simple_ascii(images):
                 # print("avg val is " + str(avg_val))
                 if avg_val > 200:
                     # bright pixel
-                    pixel = Pixel([i, j, 0], '# ')
+                    pixel = Pixel([i, j, 0], '  ')
                     ascii_matrix[i][j] = pixel
                     img1.write('  ')
                 else:
-                    pixel = Pixel([i, j, 0], '. ')
+                    pixel = Pixel([i, j, 0], '# ')
                     ascii_matrix[i][j] = pixel
                     img1.write('# ')
             img1.write('\n')
@@ -65,32 +87,44 @@ def simple_ascii(images):
         # print_inconsole(rotated_matrix)
         # print(rotated_matrix)
 
+    return ascii_matrix
 
 
-def uh(matrix):
+
+def uh(matrix):  
     cos_theta = round(cos(np.pi/4), 4) # 45 degrees
     sin_theta = round(sin(np.pi/4), 4)
     Rx_matrix = [[1,      0,          0     ],
                  [0, cos_theta, -1*sin_theta],
                  [0, sin_theta, cos_theta  ]]
     
-    column_vectors = list()
-    # add third dimension
+    # column_vectors = list()
+    # # add third dimension
+    # for i in range(len(matrix)):
+    #     for j in range(len(matrix[0])):
+    #         column_vectors.append([i, j, 0])
+
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            column_vectors.append([i, j, 0])
+            for x in range(len(Rx_matrix)):
+                points = matrix[i, j].coords
+                print(f'prev coords: {points}')
+                c = dot_product(points, Rx_matrix[x])
+                print(f'new coord: {c}')
+                matrix[i, j].set_coords(c)
 
-    transformed_vectors = list()
-    for x in column_vectors:
-        for i in range(len(Rx_matrix)):
-            c = dot_product(Rx_matrix[i], x)
-            transformed_vectors.append(c)
-            print(f'prev coord: {x}\nnew coord: {c}')
+    # transformed_vectors = list()
+    # for x in column_vectors:
+    #     for i in range(len(Rx_matrix)):
+    #         c = dot_product(Rx_matrix[i], x)
+    #         transformed_vectors.append(c)
+    #         print(f'prev coord: {x}\nnew coord: {c}')
     # ctr = 0
     # for i in range(len(matrix)):
     #     for j in range(len(matrix[0])):
     #         matrix[i, j] = transformed_vectors[ctr]
     #         ctr += 1
+    # return transformed_vectors
 
 def dot_product(vec_1: list, vec_2: list) -> list:
     if len(vec_1) != len(vec_2):
@@ -102,11 +136,43 @@ def dot_product(vec_1: list, vec_2: list) -> list:
     
     return vec
 
+def freebird(matrix):
+    '''
+    create a new matrix
+    traverse the given matrix
+    put the characters in their new coords
+    '''
+    new_matrix = np.empty((len(matrix), len(matrix[0])), dtype=Pixel)
+    for i, row in enumerate(matrix):
+        for j, element in enumerate(row):
+            coords = element.coords
+            x = int(coords[0])
+            y = int(coords[1])
+            if x < len(new_matrix) and y < len(new_matrix[0]):
+                new_matrix[x, y] = element
+                # print(f'change coords from ({i},{j}) to ({x},{y})')
+
+            else:
+                print("oops. out of bounds")
+
+    return new_matrix
+
 def print_inconsole(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            print(matrix[i][j], end="")
+            print(matrix[i, j].char, end="")
         print()
+
+def write_to_file(matrix):
+    with open("img1.txt", "w") as f:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                # if matrix[i, j] is None:
+                #     f.write('  ')
+                #     print("it's none")
+                # else:
+                f.write(matrix[i, j].char)
+            f.write('\n')
 
 def img_plot(images: list):
     fig, ax = plt.subplots(2, 3)
@@ -126,4 +192,10 @@ def img_plot(images: list):
 if __name__ == '__main__':
     # i guess i'd rather public static void main string args
     images = get_them_imgs("images")
-    simple_ascii(images)
+    matrix = simple_ascii(images)
+    print_inconsole(matrix)
+    uh(matrix)
+    # new_matrix = freebird(matrix)
+    # write_to_file(new_matrix)
+    # print_inconsole(matrix)
+
