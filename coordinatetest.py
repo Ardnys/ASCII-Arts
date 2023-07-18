a546ed0fcbf48cc331c612b8a90044137d6fac1a
Ardnys <semresavas35@gmail.com>
2023-07-18 12:47:17 +0300

    cleanup

--- coordinatetest.py
+++ coordinatetest.py
@@ -57,45 +57,32 @@ def calculate_coords(mat):
     # when you subtract, the x and y are switched places
     for i in range(len(m)):
         for j in range(len(m[0])):
-            x, y = i - 25, j - 25
+            x, y = i - vmid, j - hmid 
             m[i, j].set_coords((y, x, 0))
 
 
 def rotate(matrix):
     cos_theta = round(cos(np.pi/4), 4)  # 45 degrees
     sin_theta = round(sin(np.pi/4), 4)
-    Rx_matrix = np.array([[1,         0,          0],
-                          [0, cos_theta, -sin_theta],
-                          [0, sin_theta,  cos_theta]])
-    Ry_matrix = np.array([[cos_theta, 0,  sin_theta],
-                          [0,         1,          0],
+    Rx_matrix = np.array([[1,          0,          0],
+                          [0,  cos_theta, -sin_theta],
+                          [0,  sin_theta,  cos_theta]])
+    Ry_matrix = np.array([[cos_theta,  0,  sin_theta],
+                          [0,          1,          0],
                           [-sin_theta, 0, cos_theta]])
-    Rz_matrix = np.array([[cos_theta,-sin_theta,  0],
-                          [sin_theta, cos_theta,  0],
-                          [0,          0,         1]])
+    Rz_matrix = np.array([[ cos_theta,-sin_theta,  0],
+                          [ sin_theta, cos_theta,  0],
+                          [ 0,          0,         1]])
+    M = Rz_matrix
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            for x in range(len(Rz_matrix)):
+            for x in range(len(M)):
                 points = np.asarray(list(matrix[i, j].coords))
 
-                rotated_coords = np.dot(Rz_matrix, points)
+                rotated_coords = np.dot(M, points)
                 rotated_coords = np.around(rotated_coords)
                 rotated_coords = rotated_coords.astype(int)
-                # print(f'prev indexes: {points}')
-                # c = dot_product(points, Rx_matrix[x])
-                # print(f'new coord: {c}')
                 matrix[i, j].set_rotated_coords(rotated_coords)
-
-
-def dot_product(vec_1: tuple, vec_2: list) -> tuple:
-    if len(vec_1) != len(vec_2):
-        raise Exception("can't dot product different size vectors")
-        # i don't even know if it's true but it feels like it
-    vec = list()
-    for i, j in zip(vec_1, vec_2):
-        vec.append(int(i * j))
-
-    return tuple(vec)
 
 
 def print_inconsole(matrix):
@@ -108,7 +95,8 @@ def print_inconsole(matrix):
 def calc_indexes(matrix):
     nrows = len(matrix)
     ncols = len(matrix[0])
-
+    vmid = int(nrows/2)
+    hmid = int(ncols/2)
     ascii_matrix = np.empty((nrows, ncols), dtype=Point)
 
     for i in range(nrows):
@@ -116,7 +104,7 @@ def calc_indexes(matrix):
             p = matrix[i, j]
             rx = p.rotated_coords[0]
             ry = p.rotated_coords[1]
-            x, y = rx + 25, ry + 25
+            x, y = rx + vmid, ry + hmid 
             if x >= 0 and x < nrows and y >= 0 and y < ncols:
                 ascii_matrix[y, x] = Point(p.char, (y, x))
             else:
@@ -138,13 +126,16 @@ img = iio.imread("images\\notnotathreat.png")
 m = create_point_matrix(img)
 # print_inconsole(m)
 calculate_coords(m)
-m[25, 25].char = '$ '  # this is the origin if you will
+# m[25, 25].char = '$ '  # this is the origin if you will
 print_inconsole(m)
 rotate(m)
 
-for i in range(5):
-    for j in range(5):
-        p = m[i, j]
-        print(f'coords: {p.coords} -> rotated coords: {p.rotated_coords}')
-
 calc_indexes(m)
+
+
+
+# for i in range(5):
+#     for j in range(5):
+#         p = m[i, j]
+#         print(f'coords: {p.coords} -> rotated coords: {p.rotated_coords}')
+
