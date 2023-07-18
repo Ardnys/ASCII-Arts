c30770cbda9a0a1cae12f1286ac0ce4625e94112
Ardnys <semresavas35@gmail.com>
2023-07-18 13:33:17 +0300

    add animation!

--- coordinatetest.py
+++ coordinatetest.py
@@ -1,7 +1,10 @@
 import numpy as np
 import imageio.v3 as iio
 import sys
+import os
+import time
 from math import cos, sin
+from copy import deepcopy
 np.set_printoptions(threshold=sys.maxsize)
 
 """
@@ -26,6 +29,21 @@ class Point:
 
     def set_rotated_coords(self, r_coords):
         self.rotated_coords = r_coords
+
+def create_frames(img):
+    default_matrix = create_point_matrix(img)
+    rotations = np.linspace(0, np.pi*2, 15)
+    # frames = list()
+
+    for rotation in rotations:
+        mat = deepcopy(default_matrix)
+        calculate_coords(mat)
+        rotate(mat, rotation)
+        mat = calc_indexes(mat)
+        print_inconsole(mat)
+        time.sleep(1)
+        clear_console()
+
 
 
 def create_point_matrix(img):
@@ -55,15 +73,15 @@ def calculate_coords(mat):
     print(f'vertical: {vmid}, horizontal: {hmid}')
 
     # when you subtract, the x and y are switched places
-    for i in range(len(m)):
-        for j in range(len(m[0])):
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
             x, y = i - vmid, j - hmid 
-            m[i, j].set_coords((y, x, 0))
+            mat[i, j].set_coords((y, x, 0))
 
 
-def rotate(matrix):
-    cos_theta = round(cos(np.pi/4), 4)  # 45 degrees
-    sin_theta = round(sin(np.pi/4), 4)
+def rotate(matrix, angle):
+    cos_theta = round(cos(angle), 4)  # 45 degrees
+    sin_theta = round(sin(angle), 4)
     Rx_matrix = np.array([[1,          0,          0],
                           [0,  cos_theta, -sin_theta],
                           [0,  sin_theta,  cos_theta]])
@@ -73,7 +91,7 @@ def rotate(matrix):
     Rz_matrix = np.array([[ cos_theta,-sin_theta,  0],
                           [ sin_theta, cos_theta,  0],
                           [ 0,          0,         1]])
-    M = Rz_matrix
+    M = Ry_matrix
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             for x in range(len(M)):
@@ -83,14 +101,6 @@ def rotate(matrix):
                 rotated_coords = np.around(rotated_coords)
                 rotated_coords = rotated_coords.astype(int)
                 matrix[i, j].set_rotated_coords(rotated_coords)
-
-
-def print_inconsole(matrix):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            print(matrix[i, j].char, end="")
-        print()
-
 
 def calc_indexes(matrix):
     nrows = len(matrix)
@@ -107,30 +117,53 @@ def calc_indexes(matrix):
             x, y = rx + vmid, ry + hmid 
             if x >= 0 and x < nrows and y >= 0 and y < ncols:
                 ascii_matrix[y, x] = Point(p.char, (y, x))
-            else:
-                print("point out of bounds")
+
+    return ascii_matrix
+            
             # print(p.char, end="")
         # print()
 
-    for i in range(nrows):
-        for j in range(ncols):
-            if ascii_matrix[i, j] is None:
-                ascii_matrix[i, j] = Point("  ", (i, j))
+    # for i in range(nrows):
+    #     for j in range(ncols):
+    #         if ascii_matrix[i, j] is None:
+    #             ascii_matrix[i, j] = Point("  ", (i, j))
+    #             print("  ", end="")
+    #         else:
+    #             print(ascii_matrix[i, j].char, end="")
+    #     print()
+
+def print_inconsole(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i, j] is None:
+                matrix[i, j] = Point("  ", (i, j))
                 print("  ", end="")
             else:
-                print(ascii_matrix[i, j].char, end="")
+                print(matrix[i, j].char, end="")
         print()
 
+def clear_console():
+    # Windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+    # Linux and macOS
+    else:
+        _ = os.system('clear')
 
-img = iio.imread("images\\notnotathreat.png")
-m = create_point_matrix(img)
+
+img = iio.imread("images\\notathreat50.png")
+create_frames(img)
+
+
+# m = create_point_matrix(img)
+# # print_inconsole(m)
+# calculate_coords(m)
+# # m[25, 25].char = '$ '  # this is the origin if you will
+# # print_inconsole(m)
+# rotate(m, np.pi/4)
+
+# m = calc_indexes(m)
 # print_inconsole(m)
-calculate_coords(m)
-# m[25, 25].char = '$ '  # this is the origin if you will
-print_inconsole(m)
-rotate(m)
-
-calc_indexes(m)
 
 
 
