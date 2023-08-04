d2a0ec1c9969a411b74c5b3ab5b5d6903155438f
Ardnys <98890641+Ardnys@users.noreply.github.com>
2023-08-04 12:14:22 +0300

    matrix multip half done & nicer mat print

--- intmatrix.c
+++ intmatrix.c
@@ -3,68 +3,62 @@
 
 #include "intmatrix.h"
 
-void initIntMatrix(IntMatrix *matrix, int r, int c, int *vals, int size)
-{
+void initIntMatrix(IntMatrix* matrix, int r, int c, int* vals, int size) {
 	matrix->rows = r;
 	matrix->cols = c;
 	size_t s = (long long)r * (long long)c;
-	matrix->values = (int *)calloc(s, sizeof(int));
+	matrix->values = (int*)calloc(s, sizeof(int));
 
-	if (matrix->values == NULL)
-	{
+	if (matrix->values == NULL) {
 		printf("oof malloc didn't work\n");
 	}
-	else
-	{
-		for (size_t i = 0; i < size; i++)
-		{
+	else {
+		for (size_t i = 0; i < size; i++) {
 			matrix->values[i] = vals[i];
 		}
 	}
 }
 
-int get(IntMatrix *matrix, int i, int j)
-{
-	if (i < 0 || j < 0)
-	{
+int get(IntMatrix* matrix, int i, int j) {
+	if (i < 0 || j < 0) {
 		fprintf(stderr, "can't access negative array index!\n");
 		return -1;
 	}
-	else if (i >= matrix->rows || j >= matrix->cols)
-	{
+	else if (i >= matrix->rows || j >= matrix->cols) {
 		fprintf(stderr, "can't access out of bounds array index!\n");
 		return -1;
 	}
-	else
-	{
+	else {
 		return matrix->values[i * matrix->rows + j];
 	}
+
 }
-int *getRow(IntMatrix *matrix, int *row, int i)
-{
+int* getRow(IntMatrix* matrix, int* row, int i) {
+
 }
 
-int *getCol(IntMatrix *matrix, int *col, int j)
-{
+int* getCol(IntMatrix* matrix, int* col, int j) {
+
 }
 
-void freeValues(IntMatrix *matrix)
-{
+void freeValues(IntMatrix* matrix) {
 	free(matrix->values);
 }
 
-void print(IntMatrix *matrix)
-{
-	if (matrix->values == NULL)
-	{
+void print(IntMatrix* matrix) {
+	if (matrix->values == NULL) {
 		printf("matrix uninitialized\n");
 		return;
 	}
-	else
-	{
-		for (int i = 0; i < matrix->rows * matrix->cols; i++)
+	else {
+		for (int i = 0; i < matrix->rows; i++)
 		{
-			printf("%d ", matrix->values[i]);
+			for (int j = 0; j < matrix->cols; j++) {
+				printf("%d ", matrix->values[i * matrix->cols + j]);
+			}
+			printf("\n");
+			
 		}
 	}
+	printf("\n");
 }
